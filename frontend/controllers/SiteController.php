28b87e9b3ca5f615d9e41f37443873d3b7e985a9
Francis Njambi <fnjambi@outlook.com>
2026-06-12 09:24:31 +0300

    Fix: siteController/signup form to include appropriate scenarios and variable initialization like staff context signup

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -230,14 +230,19 @@ class SiteController extends Controller
      *
      * @return mixed
      */
-    public function actionSignup()
+     public function actionSignup()
     {
         $this->layout = 'auth';
         $model = new SignupForm();
+        if (Yii::$app->request->get('sreg')) {
+            $model->scenario = 'staff';
+            $model->staffRegistration = true;
+        }
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
             Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
             return $this->goHome();
         }
+
 
         return $this->render('signup', [
             'model' => $model,
