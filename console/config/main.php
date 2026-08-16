5d65aed0515e575521cbabba10a2cbf78a8bf5d9
Francis Njambi <fnjambi@outlook.com>
2026-08-16 19:28:59 +0300

    icons

--- console/config/main.php
+++ console/config/main.php
@@ -50,6 +50,26 @@ return [
                 ],
             ],
         ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default.
+            'useFileTransport' => false,
+            // SMTP server example:
+            'transport' => [
+                'scheme' => 'smtp',
+                'host' => env('SMTP_HOST'),
+                'username' => env('SMTP_USERNAME'),
+                'password' => env('SMTP_PASSWORD'),
+                'port' => env('SMTP_PORT'),
+                // 'dsn' => 'native://default',
+            ],
+            // DSN example:
+            //  'transport' => [
+            //      'dsn' => 'smtp://' . env('SMTP_USERNAME') . ':' . env('SMTP_PASSWORD') . '@' . env('SMTP_HOST') . ':25',
+            //   ],
+
+        ],
     ],
     'params' => $params,
 ];
